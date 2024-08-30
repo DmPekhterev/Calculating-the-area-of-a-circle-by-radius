6135606e60f86a387d71aaaaac4a3279bff29d3c
Dmitrii Pekhterev <dmpehterev@gmail.com>
2024-08-30 17:10:25 +0300

    Calculating the area of a circle by radius

---
+++ (CircleArea ) - Variables and constants problem and solution/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  (CircleArea ) - Variables and constants problem and solution
+//
+//  Created by Дмитрий on 30.08.2024.
+//
+
+import Foundation
+
+let pi: Double = 3.14
+
+var radius: Double = 5
+
+var area: Double = pi * radius * radius
+
+print(area)
+
+
+radius = 10
+
+area = pi * radius * radius
+
+print(area)
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+Solution of the educational problem - (Calculating the area of ​​a circle knowing its radius)
+
